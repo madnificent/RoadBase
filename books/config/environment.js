81701d03d1d9fccb8024a7be98cafae852308009
Aad Versteden <madnificent@gmail.com>
2021-07-12 13:15:12 +0200

    Update rootURL setting for ember serve
    Probably a snafu from my end earlier on.  This should be set to '/' to
    make root negotiation work.  Otherwise ember serve will assume the root
    URL is where you start from and thus it will request `/books/vendor.js`
    instead of `/vendor.js`.

--- books/config/environment.js
+++ books/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'books',
     environment,
-    rootURL: null,
+    rootURL: "/",
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
